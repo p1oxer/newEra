3c57ec9c1b2c03dbcaa94d41ed87b582f6b446a4
p1oxer <pabonna@mail.ru>
2025-02-07 01:05:38 +0300

    dynamic quests routes, attributes

--- src/functions/translit.js
+++ src/functions/translit.js
@@ -51,3 +51,12 @@ export function translit(word) {
     answer = answer.replace(/^\-|-$/g, "");
     return answer;
 }
+
+export function capitalizeFirstLetterAndReplaceDash(str) {
+    if (!str) return "";
+    const words = str.split("-");
+    const capitalizedWords = words.map(
+        (word) => word.charAt(0).toUpperCase() + word.slice(1)
+    );
+    return capitalizedWords.join(" ");
+}
